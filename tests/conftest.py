98191b34d1465b84d7502cedb60f6191ca27b461
Miki Tebeka <miki.tebeka@gmail.com>
2018-12-17 11:40:29 +0200

    export fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
 from os import environ
 from os.path import abspath, dirname
 
+import pytest
+
+import nuclio
+
 here = dirname(abspath(__file__))
 environ['ENV_FILE'] = '{}/env.txt'.format(here)
+
+
+@pytest.fixture
+def clean_handlers():
+    nuclio.export.handlers.clear()
