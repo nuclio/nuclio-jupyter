c0207637fa44175bcb3c32ca79a2faebcef35ff5
Miki Tebeka <miki.tebeka@gmail.com>
2019-01-31 18:49:46 +0200

    version 0.6.12

--- nuclio/__init__.py
+++ nuclio/__init__.py
@@ -16,7 +16,7 @@ from .magic import print_handler_code # noqa
 from .request import Context, Event, inject_context as _inject_context # noqa
 from . import magic  # noqa
 
-__version__ = '0.6.11'
+__version__ = '0.6.12'
 
 _inject_context()
 del _inject_context
