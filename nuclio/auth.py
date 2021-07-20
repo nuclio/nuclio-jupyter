7ed3ab375a35883f8563cc09574e1b21eb4600ff
Liran BG <liranbg@gmail.com>
2021-07-20 15:26:59 +0300

    update

--- nuclio/auth.py
+++ nuclio/auth.py
@@ -3,12 +3,17 @@ import os
 import requests.auth
 
 
+class AuthKinds:
+    nop = "nop"
+    iguazio = "iguazio"
+
+
 class AuthInfo:
     """
     Accommodate Iguazio-based authentication mechanism
     """
 
-    def __init__(self, username="", password="", mode="nop"):
+    def __init__(self, username="", password="", mode=AuthKinds.nop):
         self._username = username
         self._password = password
         self._mode = mode
@@ -17,11 +22,11 @@ class AuthInfo:
     def from_envvar(cls):
         access_key = os.getenv("V3IO_ACCESS_KEY")
         if access_key:
-            return cls(password=access_key, mode="iguazio")
-        return cls(mode="nop")
+            return cls(password=access_key, mode=AuthKinds.iguazio)
+        return cls(mode=AuthKinds.nop)
 
     def to_requests_auth(self):
-        if self._mode == "iguazio":
+        if self._mode == AuthKinds.iguazio:
             return IguazioBasicAuthRequests(self._username, self._password)
         elif self._username or self._password:
             return requests.auth.HTTPBasicAuth(self._username, self._password)
