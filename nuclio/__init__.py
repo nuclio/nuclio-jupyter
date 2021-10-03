5d88cc8b14f61ce5107dc350d216527306ee664d
Liran BG <liranbg@gmail.com>
2021-10-03 15:34:43 +0300

    bump 0.8.21

--- nuclio/__init__.py
+++ nuclio/__init__.py
@@ -19,7 +19,7 @@ from .config import Volume, ConfigSpec  # noqa
 from .build import build_file  # noqa
 from .triggers import HttpTrigger, CronTrigger, KafkaTrigger  # noqa
 
-__version__ = '0.8.20'
+__version__ = '0.8.21'
 
 _inject_context()
 del _inject_context
