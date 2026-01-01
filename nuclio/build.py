e7d8fe433bd7e2ba0c9710ed0079997b5abfd49e
royischoss <royishoss@gmail.com>
2026-01-01 13:51:02 +0200

    change mlrun footer

--- nuclio/build.py
+++ nuclio/build.py
@@ -193,11 +193,19 @@ def build_notebook(nb_file, no_embed=False, tag="", name="", ignored_tags=""):
 
 mlrun_footer = '''
 from mlrun.runtimes import nuclio_init_hook
+import asyncio
+
 def init_context(context):
     nuclio_init_hook(context, globals(), '{}')
 
 def handler(context, event):
     return context.mlrun_handler(context, event)
+
+async def async_handler(context, event):
+    mlrun_handler = context.mlrun_handler(context, event)
+    if asyncio.iscoroutine(mlrun_handler):
+        return await mlrun_handler
+    return mlrun_handler
 '''
 
 
